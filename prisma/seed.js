8bde2dcb43e99911d445e72908fe61dd74014655
Subrat Kumar Pattanaik <subratpattanaik@Subrats-MacBook-Air.local>
2023-11-18 21:31:30 +0530

    changed schema and updated seed data

--- prisma/seed.js
+++ prisma/seed.js
@@ -2,66 +2,129 @@ const { PrismaClient, Prisma } = require("@prisma/client");
 
 const prisma = new PrismaClient();
 
-// TODO: add sample data for relations here
-
-// const userData = [
-//   {
-//     name: "Alice",
-//     email: "alice@prisma.io",
-//     posts: {
-//       create: [
-//         {
-//           title: "Join the Prisma Slack",
-//           content: "https://slack.prisma.io",
-//           published: true,
-//         },
-//       ],
-//     },
-//   },
-//   {
-//     name: "Nilu",
-//     email: "nilu@prisma.io",
-//     posts: {
-//       create: [
-//         {
-//           title: "Follow Prisma on Twitter",
-//           content: "https://www.twitter.com/prisma",
-//           published: true,
-//         },
-//       ],
-//     },
-//   },
-//   {
-//     name: "Mahmoud",
-//     email: "mahmoud@prisma.io",
-//     posts: {
-//       create: [
-//         {
-//           title: "Ask a question about Prisma on GitHub",
-//           content: "https://www.github.com/prisma/prisma/discussions",
-//           published: true,
-//         },
-//         {
-//           title: "Prisma on YouTube",
-//           content: "https://pris.ly/youtube",
-//         },
-//       ],
-//     },
-//   },
-// ];
-
 async function main() {
-  console.log(`Start seeding ...`);
-  // for (const u of userData) {
-  //   const user = await prisma.user.create({
-  //     data: u,
-  //   });
-  //   console.log(`Created user with id: ${user.id}`);
-  // }
+  console.log("Started seeding..");
 
-  // TODO: iterate and create relation records here
+  // Create users
+  const user1 = await prisma.user.create({
+    data: {
+      fullName: "John Doe",
+      email: "john@example.com",
+      phone: "1234567890",
+      gender: "Male",
+      dob: "1990-01-01",
+    },
+  });
 
-  console.log(`Seeding finished.`);
+  const user2 = await prisma.user.create({
+    data: {
+      fullName: "Jane Doe",
+      email: "jane@example.com",
+      phone: "9876543210",
+      gender: "Female",
+      dob: "1995-05-15",
+    },
+  });
+
+  const user3 = await prisma.user.create({
+    data: {
+      fullName: "Nun Doe",
+      gender: "Female",
+      dob: "1998-04-25",
+    },
+  });
+
+  const user4 = await prisma.user.create({
+    data: {
+      fullName: "Pun Doe",
+      gender: "Other",
+      dob: "1999-01-03",
+    },
+  });
+
+  // Create merchants
+  const merchant1 = await prisma.merchant.create({
+    data: {
+      name: "Best Mart",
+      adminId: "best-mart",
+      displayAddress: "123 Main Street, Cityville",
+    },
+  });
+
+  const merchant2 = await prisma.merchant.create({
+    data: {
+      name: "Super Deals",
+      adminId: "super-deals",
+      displayAddress: "456 Broad Street, Townsville",
+    },
+  });
+
+  // Create products
+  const product1 = await prisma.product.create({
+    data: {
+      merchantId: merchant1.id,
+      name: "Product A",
+      price: 19.99,
+      availabilityStatus: "IN_STOCK",
+    },
+  });
+
+  const product2 = await prisma.product.create({
+    data: {
+      merchantId: merchant2.id,
+      name: "Product B",
+      price: 29.99,
+      availabilityStatus: "OUT_OF_STOCK",
+    },
+  });
+
+  // Create orders
+  const order1 = await prisma.order.create({
+    data: {
+      userId: user1.id,
+      status: "Active",
+    },
+  });
+
+  const order2 = await prisma.order.create({
+    data: {
+      userId: user2.id,
+      status: "Success",
+    },
+  });
+
+  // Create order items
+  await prisma.orderItem.createMany({
+    data: [
+      { orderId: order1.id, productId: product1.id, quantity: 2 },
+      { orderId: order2.id, productId: product2.id, quantity: 1 },
+    ],
+  });
+
+  // Create shipping addresses
+  const shippingAddress1 = await prisma.shippingAddress.create({
+    data: {
+      userId: user1.id,
+      name: user1.name,
+      email: user1.email,
+      addressLine: "789 Oak Street",
+      pincode: "12345",
+      state: "California",
+    },
+  });
+
+  const shippingAddress2 = await prisma.shippingAddress.create({
+    data: {
+      userId: user2.id,
+      name: user2.name,
+      phone: user2.phone,
+      addressLine: "567 Pine Street",
+      pincode: "67890",
+      state: "New York",
+    },
+  });
+
+  console.log("Seeding completed.");
 }
 
 main()
